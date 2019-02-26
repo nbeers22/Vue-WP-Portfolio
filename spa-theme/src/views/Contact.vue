5c688cf1f860f33ade1bee1e79d4f5e471f50d93
Nate Beers <nbeers22@gmail.com>
2019-02-26 19:04:02 +0100

    Added Form validation to contact form

--- spa-theme/src/views/Contact.vue
+++ spa-theme/src/views/Contact.vue
@@ -6,19 +6,39 @@
         <div class="col-md-10 col-lg-8">
           <div class="row form-container">
             <div class="col-md-6">
-              <input type="text" name="first_name" class="form-control form-control-lg" placeholder="First Name">
+              <div class="form-group">
+                <input type="text" name="first_name" class="form-control form-control-lg" placeholder="First Name *" @keyup="validateInput" required>
+                <img id="valid" src="/wp-content/themes/vue-spa/images/check-circle-solid.png" alt="Valid">
+                <img id="invalid" src="/wp-content/themes/vue-spa/images/times-circle-solid.png" alt="Not Valid">
+              </div>
             </div>
             <div class="col-md-6">
-              <input type="text" name="last_name" class="form-control form-control-lg" placeholder="Last Name">
+              <div class="form-group">
+                <input type="text" name="last_name" class="form-control form-control-lg" placeholder="Last Name *" @keyup="validateInput" required>
+                <img id="valid" src="/wp-content/themes/vue-spa/images/check-circle-solid.png" alt="Valid">
+                <img id="invalid" src="/wp-content/themes/vue-spa/images/times-circle-solid.png" alt="Not Valid">
+              </div>
             </div>
             <div class="col-md-6">
-              <input type="email" name="email" class="form-control form-control-lg" placeholder="Email">
+              <div class="form-group">
+                <input type="email" name="email" class="form-control form-control-lg" placeholder="Email *" required @keyup="validateInput">
+                <img id="valid" src="/wp-content/themes/vue-spa/images/check-circle-solid.png" alt="Valid">
+                <img id="invalid" src="/wp-content/themes/vue-spa/images/times-circle-solid.png" alt="Not Valid">
+              </div>
             </div>
             <div class="col-md-6">
-              <input type="text" name="company" class="form-control form-control-lg" placeholder="Company (optional)">
+              <div class="form-group">
+                <input type="text" name="company" class="form-control form-control-lg" placeholder="Company (optional)" @keyup="validateInput">
+                <img id="valid" src="/wp-content/themes/vue-spa/images/check-circle-solid.png" alt="Valid">
+                <img id="invalid" src="/wp-content/themes/vue-spa/images/times-circle-solid.png" alt="Not Valid">
+              </div>
             </div>
             <div class="col-md-12">
-              <textarea class="form-control" id="messaage" name="message" rows="6"></textarea>
+              <div class="form-group">
+                <textarea class="form-control" id="messaage" name="message" rows="6" placeholder="Message *" @keyup="validateInput" required></textarea>
+                <img id="valid" src="/wp-content/themes/vue-spa/images/check-circle-solid.png" alt="Valid">
+                <img id="invalid" src="/wp-content/themes/vue-spa/images/times-circle-solid.png" alt="Not Valid">
+              </div>
             </div>
             <div class="col-md-12 text-center">
               <input type="submit" value="Submit" class="btn btn-primary">
@@ -41,6 +61,23 @@
       NavBar,
       PageTitle
     },
+    methods:{
+      validateInput: function(event){
+        let element = event.target;
+
+        element.addEventListener("keyup", function (event) {
+          if (element.validity.valid) {
+            this.parentElement.querySelector('#invalid').classList.remove('invalid');
+            this.parentElement.querySelector('#valid').classList.add('valid');
+            this.style.border = '2px solid #42b983';
+          } else {
+            this.parentElement.querySelector('#valid').classList.remove('valid');
+            this.parentElement.querySelector('#invalid').classList.add('invalid');
+            this.style.border = '2px solid #ff0000';
+          }
+        });
+      }
+    }
 	};
 </script>
 
@@ -48,9 +85,29 @@
   .form-container{
     margin-top: 30px;
 
+    .form-control:focus{
+      box-shadow: none;
+    }
+
     .col-md-6, .col-md-12{
       padding-top: 15px;
       padding-bottom: 15px;
+    }
+
+    .form-group{
+      position: relative;
+
+      img{
+        position: absolute;
+        top: 50%;
+        transform: translateY(-50%);
+        right: 20px;
+        display: none;
+
+        &.valid,&.invalid{
+          display: block;
+        }
+      }
     }
 
     textarea{
