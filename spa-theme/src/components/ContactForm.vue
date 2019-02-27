82b093b391b0fdb05f87cbfacf3ef5533547c9d3
Nate Beers <nbeers22@gmail.com>
2019-02-27 15:51:44 +0100

    Added container to contact form

--- spa-theme/src/components/ContactForm.vue
+++ spa-theme/src/components/ContactForm.vue
@@ -1,45 +1,47 @@
 <template>
   <form action="#">
-    <div class="row justify-content-center">
-      <div class="col-md-10 col-lg-8">
-        <div class="row form-container">
-          <div class="col-md-6">
-            <div class="form-group">
-              <input type="text" name="first_name" class="form-control form-control-lg" placeholder="First Name *" @keyup="validateInput" required>
-              <img id="valid" src="/wp-content/themes/vue-spa/images/check-circle-solid.png" alt="Valid">
-              <img id="invalid" src="/wp-content/themes/vue-spa/images/times-circle-solid.png" alt="Not Valid">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-10 col-lg-8">
+          <div class="row form-container">
+            <div class="col-md-6">
+              <div class="form-group">
+                <input type="text" name="first_name" class="form-control form-control-lg" placeholder="First Name *" @keyup="validateInput" required>
+                <img id="valid" src="/wp-content/themes/vue-spa/images/check-circle-solid.png" alt="Valid">
+                <img id="invalid" src="/wp-content/themes/vue-spa/images/times-circle-solid.png" alt="Not Valid">
+              </div>
             </div>
-          </div>
-          <div class="col-md-6">
-            <div class="form-group">
-              <input type="text" name="last_name" class="form-control form-control-lg" placeholder="Last Name *" @keyup="validateInput" required>
-              <img id="valid" src="/wp-content/themes/vue-spa/images/check-circle-solid.png" alt="Valid">
-              <img id="invalid" src="/wp-content/themes/vue-spa/images/times-circle-solid.png" alt="Not Valid">
+            <div class="col-md-6">
+              <div class="form-group">
+                <input type="text" name="last_name" class="form-control form-control-lg" placeholder="Last Name *" @keyup="validateInput" required>
+                <img id="valid" src="/wp-content/themes/vue-spa/images/check-circle-solid.png" alt="Valid">
+                <img id="invalid" src="/wp-content/themes/vue-spa/images/times-circle-solid.png" alt="Not Valid">
+              </div>
             </div>
-          </div>
-          <div class="col-md-6">
-            <div class="form-group">
-              <input type="email" name="email" class="form-control form-control-lg" placeholder="Email *" required @keyup="validateInput">
-              <img id="valid" src="/wp-content/themes/vue-spa/images/check-circle-solid.png" alt="Valid">
-              <img id="invalid" src="/wp-content/themes/vue-spa/images/times-circle-solid.png" alt="Not Valid">
+            <div class="col-md-6">
+              <div class="form-group">
+                <input type="email" name="email" class="form-control form-control-lg" placeholder="Email *" required @keyup="validateInput">
+                <img id="valid" src="/wp-content/themes/vue-spa/images/check-circle-solid.png" alt="Valid">
+                <img id="invalid" src="/wp-content/themes/vue-spa/images/times-circle-solid.png" alt="Not Valid">
+              </div>
             </div>
-          </div>
-          <div class="col-md-6">
-            <div class="form-group">
-              <input type="text" name="company" class="form-control form-control-lg" placeholder="Company (optional)" @keyup="validateInput">
-              <img id="valid" src="/wp-content/themes/vue-spa/images/check-circle-solid.png" alt="Valid">
-              <img id="invalid" src="/wp-content/themes/vue-spa/images/times-circle-solid.png" alt="Not Valid">
+            <div class="col-md-6">
+              <div class="form-group">
+                <input type="text" name="company" class="form-control form-control-lg" placeholder="Company (optional)" @keyup="validateInput">
+                <img id="valid" src="/wp-content/themes/vue-spa/images/check-circle-solid.png" alt="Valid">
+                <img id="invalid" src="/wp-content/themes/vue-spa/images/times-circle-solid.png" alt="Not Valid">
+              </div>
             </div>
-          </div>
-          <div class="col-md-12">
-            <div class="form-group">
-              <textarea class="form-control" id="messaage" name="message" rows="6" placeholder="Message *" @keyup="validateInput" required></textarea>
-              <img id="valid" src="/wp-content/themes/vue-spa/images/check-circle-solid.png" alt="Valid">
-              <img id="invalid" src="/wp-content/themes/vue-spa/images/times-circle-solid.png" alt="Not Valid">
+            <div class="col-md-12">
+              <div class="form-group">
+                <textarea class="form-control" id="messaage" name="message" rows="6" placeholder="Message *" @keyup="validateInput" required></textarea>
+                <img id="valid" src="/wp-content/themes/vue-spa/images/check-circle-solid.png" alt="Valid">
+                <img id="invalid" src="/wp-content/themes/vue-spa/images/times-circle-solid.png" alt="Not Valid">
+              </div>
             </div>
-          </div>
-          <div class="col-md-12 text-center">
-            <input type="submit" value="Submit" class="btn btn-primary">
+            <div class="col-md-12 text-center">
+              <input type="submit" value="Submit" class="btn btn-primary">
+            </div>
           </div>
         </div>
       </div>
